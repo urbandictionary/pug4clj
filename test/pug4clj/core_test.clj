921ba7667016bcf33880a821f5dc1932a89afbfb
Aaron <apeckham@gmail.com>
2023-03-10 23:49:20 +0000

    improve tests

--- test/pug4clj/core_test.clj
+++ test/pug4clj/core_test.clj
@@ -1,7 +1,16 @@
 (ns pug4clj.core-test
   (:require [clojure.test :refer :all]
-            [pug4clj.core :refer :all])
+            [pug4clj.core :refer :all]
+            [clojure.walk :refer [stringify-keys]])
   (:import (de.neuland.pug4j Pug4J)))
 
 (deftest a-test
-  (is (re-find #"<title>Example Domain</title>" (Pug4J/render "./index.pug" {}))))
+  (let [actual (Pug4J/render
+                 "./index.pug"
+                 (stringify-keys
+                   {:pageName "list of <blink>books</blink>",
+                    :books
+                      [{:available true, :name "available=yes", :price "1"}
+                       {:available false, :name "available=no", :price "0"}]}))]
+    (is (re-find #"available=yes" actual))
+    (is (not (re-find #"available=no" actual)))))
