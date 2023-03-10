0c6a67ff2b6f7e5176a8cf4e9b96e89a8ed97411
Aaron <apeckham@gmail.com>
2023-03-10 23:43:09 +0000

    example.com

--- test/pug4clj/core_test.clj
+++ test/pug4clj/core_test.clj
@@ -1,7 +1,7 @@
 (ns pug4clj.core-test
   (:require [clojure.test :refer :all]
-            [pug4clj.core :refer :all]))
+            [pug4clj.core :refer :all])
+  (:import (de.neuland.pug4j Pug4J)))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (is (re-find #"<title>Example Domain</title>" (Pug4J/render "./index.pug" {}))))
