62be4f902254db13901a58371d33bfec599728f6
Aaron <apeckham@gmail.com>
2023-03-10 23:53:24 +0000

    move to resources dir so it can work inside a jar

--- test/pug4clj/core_test.clj
+++ test/pug4clj/core_test.clj
@@ -1,16 +1,17 @@
 (ns pug4clj.core-test
   (:require [clojure.test :refer :all]
             [pug4clj.core :refer :all]
+            [clojure.java.io :as io]
             [clojure.walk :refer [stringify-keys]])
   (:import (de.neuland.pug4j Pug4J)))
 
 (deftest a-test
   (let [actual (Pug4J/render
-                 "./index.pug"
+                 (io/resource "index.pug")
                  (stringify-keys
                    {:pageName "list of <blink>books</blink>",
                     :books
-                      [{:available true, :name "available=yes", :price "1"}
+                      [{:available true, :name "available=yes", :price 1}
                        {:available false, :name "available=no", :price "0"}]}))]
     (is (re-find #"available=yes" actual))
     (is (not (re-find #"available=no" actual)))))
