32e17e1a1c5e265fb033f2a098398fd82ba630b9
Aaron <apeckham@gmail.com>
2023-03-22 02:26:18 +0000

    format

--- src/pug4clj/core.clj
+++ src/pug4clj/core.clj
@@ -8,22 +8,20 @@
 (def resource-template-loader
   (reify
     TemplateLoader
-    (getLastModified [_ name]
-      (.getLastModified (.openConnection (io/resource name))))
-    (getReader [_ name] (io/reader (io/resource name)))))
+      (getLastModified [_ name]
+        (.getLastModified (.openConnection (io/resource name))))
+      (getReader [_ name] (io/reader (io/resource name)))))
 
 (defn render
   []
   (let [config (doto (PugConfiguration.)
                  (.setTemplateLoader resource-template-loader))]
     (.renderTemplate
-     config
-     (.getTemplate config "index.pug")
-     (stringify-keys
-      {:pageName "list of <blink>books</blink>",
-       :books [{:available true, :name "available=yes", :price 1}
-               {:available false, :name "available=no", :price "0"}]}))))
+      config
+      (.getTemplate config "index.pug")
+      (stringify-keys
+        {:pageName "list of <blink>books</blink>",
+         :books [{:available true, :name "available=yes", :price 1}
+                 {:available false, :name "available=no", :price "0"}]}))))
 
-(defn -main
-  [& args]
-  (println (render)))
+(defn -main [& args] (println (render)))
