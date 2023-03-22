7b61e072897066ff5de15c15a7a998a0eaf38954
Aaron <apeckham@gmail.com>
2023-03-22 02:13:55 +0000

    use full API

--- test/pug4clj/core_test.clj
+++ test/pug4clj/core_test.clj
@@ -3,16 +3,18 @@
             [pug4clj.core :refer :all]
             [clojure.java.io :as io]
             [clojure.walk :refer [stringify-keys]])
-  (:import (de.neuland.pug4j Pug4J)))
+  (:import (de.neuland.pug4j PugConfiguration)))
 
 (deftest a-test
-  (let [actual (Pug4J/render
-                 (io/resource "index.pug")
-                 (stringify-keys
-                   {:pageName "list of <blink>books</blink>",
-                    :books
-                      [{:available true, :name "available=yes", :price 1}
-                       {:available false, :name "available=no", :price "0"}]}))]
+  (let [config (PugConfiguration.)
+        actual (.renderTemplate
+                config
+                (.getTemplate config "resources/index.pug")
+                (stringify-keys
+                 {:pageName "list of <blink>books</blink>",
+                  :books
+                  [{:available true, :name "available=yes", :price 1}
+                   {:available false, :name "available=no", :price "0"}]}))]
     (is (re-find #"available=yes" actual))
     (is (re-find #"&lt;blink&gt;" actual))
     (is (not (re-find #"available=no" actual)))))
