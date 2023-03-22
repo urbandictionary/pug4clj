aeb91a74558bda20be96cec1112b5df81e0342e3
Aaron <apeckham@gmail.com>
2023-03-22 02:21:53 +0000

    extract

--- src/pug4clj/core.clj
+++ src/pug4clj/core.clj
@@ -1,4 +1,27 @@
-(ns pug4clj.core)
+(ns pug4clj.core
+  (:require [clojure.java.io :as io]
+            [clojure.walk :refer [stringify-keys]])
+  (:import (de.neuland.pug4j PugConfiguration)
+           (de.neuland.pug4j.template TemplateLoader)))
+
+(def resource-template-loader
+  (reify
+    TemplateLoader
+    (getLastModified [_ name]
+      (.getLastModified (.openConnection (io/resource name))))
+    (getReader [_ name] (io/reader (io/resource name)))))
+
+(defn render
+  []
+  (let [config (doto (PugConfiguration.)
+                 (.setTemplateLoader resource-template-loader))]
+    (.renderTemplate
+     config
+     (.getTemplate config "index.pug")
+     (stringify-keys
+      {:pageName "list of <blink>books</blink>",
+       :books [{:available true, :name "available=yes", :price 1}
+               {:available false, :name "available=no", :price "0"}]}))))
 
 (defn foo
   "I don't do a whole lot."
--- test/pug4clj/core_test.clj
+++ test/pug4clj/core_test.clj
@@ -1,29 +1,6 @@
 (ns pug4clj.core-test
   (:require [clojure.test :refer :all]
-            [pug4clj.core :refer :all]
-            [clojure.java.io :as io]
-            [clojure.walk :refer [stringify-keys]])
-  (:import (de.neuland.pug4j PugConfiguration)
-           (de.neuland.pug4j.template TemplateLoader)))
-
-(def resource-template-loader
-  (reify
-    TemplateLoader
-      (getLastModified [_ name]
-        (.getLastModified (.openConnection (io/resource name))))
-      (getReader [_ name] (io/reader (io/resource name)))))
-
-(defn render
-  []
-  (let [config (doto (PugConfiguration.)
-                 (.setTemplateLoader resource-template-loader))]
-    (.renderTemplate
-      config
-      (.getTemplate config "index.pug")
-      (stringify-keys
-        {:pageName "list of <blink>books</blink>",
-         :books [{:available true, :name "available=yes", :price 1}
-                 {:available false, :name "available=no", :price "0"}]}))))
+            [pug4clj.core :refer :all]))
 
 (deftest html-test
   (let [html (render)]
