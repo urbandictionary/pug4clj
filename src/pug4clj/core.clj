fcd0a3f56157e5c8e578ffd0e2d1e6e2abfa8ae0
Aaron <apeckham@gmail.com>
2023-03-22 02:22:40 +0000

    add -main

--- src/pug4clj/core.clj
+++ src/pug4clj/core.clj
@@ -23,7 +23,6 @@
        :books [{:available true, :name "available=yes", :price 1}
                {:available false, :name "available=no", :price "0"}]}))))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  [& args]
+  (println (render)))
