de7237c59d5f586c33b293bcee535eec2220d3a6
Aaron <apeckham@gmail.com>
2023-04-02 02:43:39 +0000

    extract

--- test/pug/core_test.clj
+++ test/pug/core_test.clj
@@ -3,11 +3,15 @@
             [pug.core :refer :all]))
 
 (deftest html-test
-  (let [model {:pageName "list of <blink>books</blink>",
-               :books [{:available true, :name "available=yes", :price 1}
-                       {:available false, :name "available=no", :price "0"}]}]
-    (is (re-find #"available=yes" (render (config) "test.pug" model)))
-    (is (re-find #"&lt;blink&gt;" (render (config) "test.pug" model)))
-    (is (not (re-find #"available=no" (render (config) "test.pug" model))))))
+  (let [actual (render
+                 (config)
+                 "test.pug"
+                 {:pageName "list of <blink>books</blink>",
+                  :books
+                    [{:available true, :name "available=yes", :price 1}
+                     {:available false, :name "available=no", :price "0"}]})]
+    (is (re-find #"available=yes" actual))
+    (is (re-find #"&lt;blink&gt;" actual))
+    (is (not (re-find #"available=no" actual)))))
 
 (deftest complex-test (render (config) "complex/index.pug" {}))
