af242fb11e3da5a0f60dbe35636786aa8a34a2cc
Aaron <apeckham@gmail.com>
2023-04-02 02:49:36 +0000

    set versions etc

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 test-jar:
 	lein uberjar
-	java -jar target/pug4clj-0.1.0-SNAPSHOT-standalone.jar test.pug
-	java -jar target/pug4clj-0.1.0-SNAPSHOT-standalone.jar complex/index.pug
+	java -jar target/pug4clj-0.0.1-standalone.jar test.pug
+	java -jar target/pug4clj-0.0.1-standalone.jar complex/index.pug
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pug4clj "0.1.0-SNAPSHOT"
+(defproject com.urbandictionary/pug4clj "0.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0",
@@ -6,5 +6,10 @@
   :main ^:skip-aot pug.core
   :dependencies [[org.clojure/clojure "1.11.1"] [de.neuland-bfi/pug4j "2.0.6"]]
   :repl-options {:init-ns pug.core}
+  :deploy-repositories [["clojars"
+                         {:password :env/clojars_password,
+                          :sign-releases false,
+                          :url "https://repo.clojars.org",
+                          :username :env/clojars_username}]]
   :profiles {:uberjar {:aot :all,
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
